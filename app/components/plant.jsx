1fe64a02d0be0067d4125640aa0b3a5ed1294e1e
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2024-09-01 19:57:12 -0400

    adds garden modals

--- app/components/plant.jsx
+++ app/components/plant.jsx
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+"use client";
+import React, { Component, useState } from 'react';
 import Image from "next/image";
 
 
 export default function Plant({id, fields}) {
-
+  const [open, setOpen] = useState(false);
   
   return (
-    <div className="plant">
+    <div onClick={() => {setOpen(!open)}} className="plant">
         {(fields?.RoseSVG?.length > 0) &&
           <img alt="a unique white flower" src={fields.RoseSVG[0].url} />
         }
@@ -22,9 +23,36 @@ export default function Plant({id, fields}) {
         {fields?.OptionalLocation &&
          <p className='tiny-text'><em>{fields?.OptionalLocation}</em></p>
         }
-
-      
-
+      {open &&
+              <div className="plant-modal">
+                <div className="inner">
+                {fields?.Dedication &&
+                  <h2 className='large-text'>{fields?.Dedication}</h2>
+                }
+                {fields?.DedicationPlace &&
+                  <h2 className='large-text'><em>{fields?.DedicationPlace}</em></h2>
+                }
+                {fields?.DedicationThing &&
+                  <h2 className='large-text'><em>{fields?.DedicationThing}</em></h2>
+                }
+                {(fields?.OptionalPhoto?.length > 0) &&
+                  <img alt="a photograph of the dedicatee" src={fields.OptionalPhoto[0].url} />
+                }
+                {fields?.OptionalLocation &&
+                  <p className="small-text"><em>{fields?.OptionalLocation}</em></p>
+                }
+                {fields?.OptionalNote &&
+                  <p className="medium-text">{fields?.OptionalNote}</p>
+                }
+                 {fields?.OptionalLink &&
+                  <a target="_blank" href={fields?.OptionalLink} className="medium-text">{fields?.OptionalLink}</a>
+                }
+                 {fields?.OptionalVideoLink &&
+                  <a target="_blank" href={fields?.OptionalVideoLink} className="medium-text">{fields?.OptionalVideoLink}</a>
+                }
+                </div>
+              </div>
+      }
     </div>
   );
 }
