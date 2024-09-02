206ed6ccff5c2510f4572a127cfc0cce7dfa5941
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2024-09-02 10:25:20 -0400

    fixes env variable names for security

--- app/garden/page.jsx
+++ app/garden/page.jsx
@@ -11,7 +11,7 @@ export default function Home() {
   
   return (
     <div className="flex min-h-screen flex-col items-center justify-between">
-           <GardenGrid auth={process.env.REACT_APP_PAT}/>
+           <GardenGrid auth={process.env.PAT}/>
            <div className="about-link"><a className="medium-text" href="/about">about the garden</a></div>
            <div className="right-link">    <a href="/flower" className='medium-text-link'>plant a flower</a></div>
 
