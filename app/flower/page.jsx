ee7f2093c8f2bfaae41ffd75f733b51ffa0f9f87
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2024-09-02 11:26:16 -0400

    update env variables

--- app/flower/page.jsx
+++ app/flower/page.jsx
@@ -7,7 +7,7 @@ export default function Flower() {
 
   return (
     <>
-    <PlantFlower cloudinaryKey={process.env.CLOUDINARY_KEY} auth={process.env.PAT} secret={process.env.CLOUDINARY_SECRET} />
+    <PlantFlower cloudinaryKey={process.env.CLOUDINARY_KEY} auth={process.env.NEXT_PUBLIC_PAT} secret={process.env.CLOUDINARY_SECRET} />
     
     <div className="about-link"><a className="medium-text" href="/about">about the garden</a></div>
     <div className="right-link">    <a href="/garden" className='medium-text-link'>↩ back to the garden</a></div>
